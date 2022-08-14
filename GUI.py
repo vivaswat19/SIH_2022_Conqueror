bfbc7ef8735f1ddeab7ad96985fe10e740ca6ae0
sourabh112 <sourabhchoudhary112112@gmail.com>
2022-08-14 23:46:45 +0530

    Adding checkbox

--- GUI.py
+++ GUI.py
@@ -1,6 +1,6 @@
 import sys
 
-from PyQt6.QtCore import QSize, Qt
+from PyQt6.QtCore import *
 from PyQt6.QtWidgets import *
 
 
@@ -20,10 +20,31 @@ class MainWindow(QMainWindow):
 class Screen2(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("page2")
+        self.setHeight(1000)
+        self.setWidth(1000)
+        self.langs ={'c':False, 'cpp':False, 'java':False, 'python':False}
+        self.setWindowTitle("Pumping test Model")
         self.page_1_heading = QLabel(self)
-        self.page_1_heading.setText("Page 2")
+        self.page_1_heading.setText("Select Testing Model")
         self.page_1_heading.adjustSize()
+
+        self.checkBox_c = QCheckBox(self)
+        self.checkBox_c.setGeometry(QRect(170, 120, 81, 20))
+        self.checkBox_c.stateChanged.connect(self.checkedc)
+        self.checkBox_c.setText("C")
+
+        # self.next = QPushButton(text="Next Page", parent=self)
+        # self.next.clicked.connect(self.nextpage)  
+        
+        # self.last = QPushButton(text="Last Page", parent=self)
+        # self.last.clicked.connect(self.lastpage)        
+
+    def checkedc(self,checked):
+        self.langs['c'] = checked
+    def nextpage(self):
+        widget.setCurrentIndex(widget.currentIndex() + 1)  
+    def lastpage(self):
+        widget.setCurrentIndex(widget.currentIndex() - 1)  
 
 app = QApplication(sys.argv)
 window = MainWindow()
