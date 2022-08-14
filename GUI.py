edebd32a56c011756577c0b63ba9378e6d08d0de
sourabh112 <sourabhchoudhary112112@gmail.com>
2022-08-15 00:18:36 +0530

    checkbox

--- GUI.py
+++ GUI.py
@@ -26,39 +26,39 @@ class MainWindow(QWidget):
 class Screen2(QWidget):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("Aquifer Test")
-        layout = QGridLayout()
+        self.langs ={'T1':False, 'T2':False, 'T3':False, 'T4':False,'T5':False,'T6':False}
+        self.setWindowTitle("Pumping test Model")
 
-        self.page_1_heading = QLabel("<h1>Hydraulic Properties</h1>",self)
+        layout = QGridLayout()
+        self.page_1_heading = QLabel("<h1>Select Testing Model</h1>",self)
         layout.addWidget(self.page_1_heading, 0, 0, 1, 5)
 
-        self.aqfr_hydr_cond = QLabel("<h3>Aquifer Hydraulic Conductivity</h3>")
-        layout.addWidget(self.aqfr_hydr_cond, 1, 0, 1, 2)
+        
+        self.checkBox_t1 = QCheckBox(self)
+        self.checkBox_t1.stateChanged.connect(self.checkedc)
+        self.checkBox_t1.setText("T1")
 
-        self.button = QPushButton(text="Click", parent=self)
-        # self.button.clicked.connect(self.gotopage2)   
-        layout.addWidget(self.button, 3, 0, 1, 2)
+        layout.addWidget(self.checkBox_t1, 1, 1, 1, -1)
+        
+        self.checkBox_t2 = QCheckBox(self)
+        self.checkBox_t2.stateChanged.connect(self.checkedc)
+        self.checkBox_t2.setText("T1")
+
+        layout.addWidget(self.checkBox_t2, 2, 1, 1, -1)
+        
+        self.checkBox_t3 = QCheckBox(self)
+        self.checkBox_t3.stateChanged.connect(self.checkedc)
+        self.checkBox_t3.setText("T1")
+
+        layout.addWidget(self.checkBox_t3, 3, 1, 1, -1)
+        
+        self.checkBox_t4 = QCheckBox(self)
+        self.checkBox_t4.stateChanged.connect(self.checkedc)
+        self.checkBox_t4.setText("T1")
+
+        layout.addWidget(self.checkBox_t4, 4, 1, 1, -1)
 
         self.setLayout(layout)
-
-
-
-        # self.langs ={'c':False, 'cpp':False, 'java':False, 'python':False}
-        # self.setWindowTitle("Pumping test Model")
-
-        # layout = QGridLayout()
-        # self.page_1_heading = QLabel("<h1>Select Testing Model</h1>",self)
-        # layout.addWidget(self.page_1_heading, 0, 0, 1, 5)
-        # self.page_1_heading.adjustSize()
-
-        
-        # self.checkBox_c = QCheckBox(self)
-        # self.checkBox_c.stateChanged.connect(self.checkedc)
-        # self.checkBox_c.setText("C")
-
-        # layout.addWidget(self.page_1_heading, 1, 1, 1, 1)
-
-        # self.setLayout(layout)
 
 
         # self.next = QPushButton(text="Next Page", parent=self)
