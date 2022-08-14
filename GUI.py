c42e3e7b6e85c3c0b00214aab084d8ab21f72d6d
sourabh112 <sourabhchoudhary112112@gmail.com>
2022-08-15 00:04:56 +0530

    layout and stuff

--- GUI.py
+++ GUI.py
@@ -23,21 +23,43 @@ class MainWindow(QWidget):
     def gotopage2(self):
         widget.setCurrentIndex(widget.currentIndex() + 1)       
 
-class Screen2(QMainWindow):
+class Screen2(QWidget):
     def __init__(self):
         super().__init__()
-        self.setHeight(1000)
-        self.setWidth(1000)
-        self.langs ={'c':False, 'cpp':False, 'java':False, 'python':False}
-        self.setWindowTitle("Pumping test Model")
-        self.page_1_heading = QLabel(self)
-        self.page_1_heading.setText("Select Testing Model")
-        self.page_1_heading.adjustSize()
+        self.setWindowTitle("Aquifer Test")
+        layout = QGridLayout()
 
-        self.checkBox_c = QCheckBox(self)
-        self.checkBox_c.setGeometry(QRect(170, 120, 81, 20))
-        self.checkBox_c.stateChanged.connect(self.checkedc)
-        self.checkBox_c.setText("C")
+        self.page_1_heading = QLabel("<h1>Hydraulic Properties</h1>",self)
+        layout.addWidget(self.page_1_heading, 0, 0, 1, 5)
+
+        self.aqfr_hydr_cond = QLabel("<h3>Aquifer Hydraulic Conductivity</h3>")
+        layout.addWidget(self.aqfr_hydr_cond, 1, 0, 1, 2)
+
+        self.button = QPushButton(text="Click", parent=self)
+        # self.button.clicked.connect(self.gotopage2)   
+        layout.addWidget(self.button, 3, 0, 1, 2)
+
+        self.setLayout(layout)
+
+
+
+        # self.langs ={'c':False, 'cpp':False, 'java':False, 'python':False}
+        # self.setWindowTitle("Pumping test Model")
+
+        # layout = QGridLayout()
+        # self.page_1_heading = QLabel("<h1>Select Testing Model</h1>",self)
+        # layout.addWidget(self.page_1_heading, 0, 0, 1, 5)
+        # self.page_1_heading.adjustSize()
+
+        
+        # self.checkBox_c = QCheckBox(self)
+        # self.checkBox_c.stateChanged.connect(self.checkedc)
+        # self.checkBox_c.setText("C")
+
+        # layout.addWidget(self.page_1_heading, 1, 1, 1, 1)
+
+        # self.setLayout(layout)
+
 
         # self.next = QPushButton(text="Next Page", parent=self)
         # self.next.clicked.connect(self.nextpage)  
