bcadd7055bdc2abe42d221778d127ec03fc1fcc3
Vivaswat Sinha <vivaswat19@gmail.com>
2022-08-14 23:02:04 +0530

    base

--- GUI.py
+++ GUI.py
@@ -1,22 +1,24 @@
 import sys
 
 from PyQt6.QtCore import QSize, Qt
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
+from PyQt6.QtWidgets import *
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.page_1()
+        
 
-        self.setWindowTitle("My App")
-        button = QPushButton("Press Me!")
 
-        self.setCentralWidget(button)
+    def page_1(self):
+        self.setWindowTitle("Aquifer Test")
+        self.showMaximized()
+        self.page_1_heading = QLabel("Hydraulic Properties", self)
+        self.show()
+
 
 
 app = QApplication(sys.argv)
-
 window = MainWindow()
-window.show()
-
 app.exec()
