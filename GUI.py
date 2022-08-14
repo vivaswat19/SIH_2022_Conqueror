ff983b27517e6e77a4ca1d6459854db977c14b7d
Vivaswat Sinha <vivaswat19@gmail.com>
2022-08-15 00:16:46 +0530

    page_1 ui complete

--- GUI.py
+++ GUI.py
@@ -12,15 +12,50 @@ class MainWindow(QWidget):
         layout.addWidget(self.page_1_heading, 0, 0, 1, 5)
 
         self.aqfr_hydr_cond = QLabel("<h3>Aquifer Hydraulic Conductivity</h3>")
-        layout.addWidget(self.aqfr_hydr_cond, 1, 0, 1, 2)
+        self.aqfr_hydr_cond_input = QLineEdit(self)
+        layout.addWidget(self.aqfr_hydr_cond, 1, 0, 1, 3)
+        layout.addWidget(self.aqfr_hydr_cond_input, 1,3,1,2)
 
-        self.button = QPushButton(text="Click", parent=self)
-        self.button.clicked.connect(self.gotopage2)   
-        layout.addWidget(self.button, 3, 0, 1, 2)
+        self.aqfr_spec_storage = QLabel("<h3>Aquifer Specific Storage</h3>")
+        self.aqfr_spec_storage_input = QLineEdit(self)
+        layout.addWidget(self.aqfr_spec_storage, 2, 0, 1, 3)
+        layout.addWidget(self.aqfr_spec_storage_input, 2, 3, 1, 2)
+        
+        self.aqfr_spec_yield = QLabel("<h3>Aquifer Specific Yield</h3>")
+        self.aqfr_spec_yield_input = QLineEdit(self)
+        layout.addWidget(self.aqfr_spec_yield, 3, 0, 1, 3)
+        layout.addWidget(self.aqfr_spec_yield_input, 3, 3, 1, 2)
+        
+        self.aqfr_thickness = QLabel("<h3>Aquifer Thickness</h3>")
+        self.aqfr_thickness_input = QLineEdit(self)
+        layout.addWidget(self.aqfr_thickness, 4, 0, 1, 3)
+        layout.addWidget(self.aqfr_thickness_input, 4, 3, 1, 2)
+        
+        self.aqfrd_thickness = QLabel("<h3>Aquitard Thickness</h3>")
+        self.aqfrd_thickness_input = QLineEdit(self)
+        layout.addWidget(self.aqfrd_thickness, 5, 0, 1, 3)
+        layout.addWidget(self.aqfrd_thickness_input, 5, 3, 1, 2)
+        
+        self.aqfrd_vert_cond= QLabel("<h3>Aquitard Vertical conductivity</h3>")
+        self.aqfrd_vert_cond_input= QLineEdit(self)
+        layout.addWidget(self.aqfrd_vert_cond, 6, 0, 1, 3)
+        layout.addWidget(self.aqfrd_vert_cond_input, 6, 3, 1, 2)
+        
+        self.aqfrd_spec_storage = QLabel("<h3>Aquitard Specific storage</h3>")
+        self.aqfrd_spec_storage_input = QLineEdit(self)
+        layout.addWidget(self.aqfrd_spec_storage, 7, 0, 1, 3)
+        layout.addWidget(self.aqfrd_spec_storage_input, 7, 3, 1, 2)
+
+        self.update_btn = QPushButton(text="Update", parent=self)
+        layout.addWidget(self.update_btn, 8, 0, 1, 2)
+
+        self.save_btn = QPushButton(text="Save", parent=self)
+        self.save_btn.clicked.connect(self.next)   
+        layout.addWidget(self.save_btn, 8, 3, 1, 2)
 
         self.setLayout(layout)
 
-    def gotopage2(self):
+    def next(self):
         widget.setCurrentIndex(widget.currentIndex() + 1)       
 
 class Screen2(QMainWindow):
